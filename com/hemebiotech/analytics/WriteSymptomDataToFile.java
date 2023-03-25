accff530978e87cb6582c2668c2000a072e0f75b
ZinebELHALLAOUI <zinebhellaoui@gmail.com>
2023-03-25 12:36:14 +0100

    writer implementation

--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -2,7 +2,25 @@ package com.hemebiotech.analytics;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.Map;
 
-// WRITE YOUR CODE HERE
+public class WriteSymptomDataToFile implements ISymptomWriter {
+    private String filepath;
+
+    public WriteSymptomDataToFile(String filepath) {
+        this.filepath = filepath;
+    }
+
+    @Override
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+        try {
+            FileWriter writer = new FileWriter(filepath);
+            for (Map.Entry<String, Integer> symptomAndCounter : symptoms.entrySet()) {
+                writer.write(symptomAndCounter.getKey() + " : " + symptomAndCounter.getValue());
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
