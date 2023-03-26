5538aa367c8b72e42c20457d2faf430352106f0c
ZinebELHALLAOUI <zinebhellaoui@gmail.com>
2023-03-26 22:31:40 +0200

    refactor analytics counter

--- com/hemebiotech/analytics/AnalyticsCounter.java
+++ com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,41 +3,78 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+    private static int headacheCount = 0;
+    private static int rashCount = 0;
+    private static int pupilCount = 0;
 
-		int i = 0;
-		int headCount = 0;
-		while (line != null) {
-			i++;
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+    private ISymptomReader reader;
+    private ISymptomWriter writer;
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+    public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
+
+    public List<String> getSymptoms() {
+        return this.reader.getSymptoms();
+    }
+
+    public Map<String, Integer> countSymptoms(List<String> symptoms) {
+        Map<String, Integer> occurrences = new HashMap<>();
+        for (String symptom : symptoms) {
+
+            if (!occurrences.containsKey(symptom)) {
+                occurrences.put(symptom, 1);
+            } else {
+                occurrences.put(symptom, occurrences.get(symptom) + 1);
+            }
+
+        }
+        return occurrences;
+    }
+
+    public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+        return new TreeMap<>(symptoms);
+    }
+
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+        writer.writeSymptoms(symptoms);
+    }
+
+
+    public static void main(String args[]) throws Exception {
+        // first get input
+        BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
+        String line = reader.readLine();
+
+        int i = 0;
+        int headCount = 0;
+        while (line != null) {
+            i++;
+            System.out.println("symptom from file: " + line);
+            if (line.equals("headache")) {
+                headCount++;
+                System.out.println("number of headaches: " + headCount);
+            } else if (line.equals("rush")) {
+                rashCount++;
+            } else if (line.contains("pupils")) {
+                pupilCount++;
+            }
+
+            line = reader.readLine();    // get another symptom
+        }
+
+        // next generate output
+        FileWriter writer = new FileWriter("result.out");
+        writer.write("headache: " + headacheCount + "\n");
+        writer.write("rash: " + rashCount + "\n");
+        writer.write("dialated pupils: " + pupilCount + "\n");
+        writer.close();
+    }
 }
--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -5,18 +5,13 @@ import java.io.IOException;
 import java.util.Map;
 
 public class WriteSymptomDataToFile implements ISymptomWriter {
-    private String filepath;
-
-    public WriteSymptomDataToFile(String filepath) {
-        this.filepath = filepath;
-    }
 
     @Override
     public void writeSymptoms(Map<String, Integer> symptoms) {
         try {
-            FileWriter writer = new FileWriter(filepath);
+            FileWriter writer = new FileWriter("result.out");
             for (Map.Entry<String, Integer> symptomAndCounter : symptoms.entrySet()) {
-                writer.write(symptomAndCounter.getKey() + " : " + symptomAndCounter.getValue());
+                writer.write(symptomAndCounter.getKey() + " : " + symptomAndCounter.getValue() + "\n");
             }
             writer.close();
         } catch (IOException e) {
