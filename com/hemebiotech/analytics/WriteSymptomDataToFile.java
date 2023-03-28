e37f1fb83e01934723469d0a69b0d17adc68bede
ZinebELHALLAOUI <zinebhellaoui@gmail.com>
2023-03-29 01:14:04 +0200

    clean code, add javadoc

--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -4,18 +4,21 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map;
 
+/**
+ * Implementation of ISymptomWriter, this implementation concerns writing data to file output.
+ */
 public class WriteSymptomDataToFile implements ISymptomWriter {
 
     @Override
     public void writeSymptoms(Map<String, Integer> symptoms) {
-        try {
-            FileWriter writer = new FileWriter("result.out");
+        try (FileWriter writer = new FileWriter("result.out");) {
+
             for (Map.Entry<String, Integer> symptomAndCounter : symptoms.entrySet()) {
                 writer.write(symptomAndCounter.getKey() + " : " + symptomAndCounter.getValue() + "\n");
             }
-            writer.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Erreur lors de l'écriture du résultat" );
         }
+
     }
 }
